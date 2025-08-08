427020fc949399ae0c222d43d0569f5c022808fe
Nail Minnigalin <minnigalin.ni@finist-soft.ru>
2025-08-08 15:10:18 +0300

    Implement task controller

---
+++ TMPTaskService/Controllers/TaskController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TMPTaskService.Domain.Interfaces;
+
+namespace TMPTaskService.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class TaskController : Controller
+	{
+		public TaskController(ITaskManager taskManager)
+		{
+			_taskManager = taskManager;
+		}
+
+		private readonly ITaskManager _taskManager;
+
+		[HttpPost]
+		public async Task<IActionResult> CreateTask(string name, string? description)
+		{
+			await _taskManager.CreateTaskAsync(name, description);
+			return Ok();
+		}
+	}
+}
---
+++ TMPTaskServiceUnitTest/TaskControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TMPTaskService.Controllers;
+using TMPTaskService.Domain.Interfaces;
+
+namespace TMPTaskServiceUnitTest
+{
+	public class TaskControllerTests
+	{
+		[Fact]
+		public async Task CreateTask_Return_Ok_When_Task_Created()
+		{
+			var mockTaskManager = new Mock<ITaskManager>();
+			mockTaskManager.Setup(m => m.CreateTaskAsync(It.IsAny<string>(), It.IsAny<string>()));
+			TaskController taskController = new(mockTaskManager.Object);
+
+			var result = await taskController.CreateTask("TestTask", "TestDescription");
+
+			Assert.IsType<OkResult>(result);
+		}
+
+		[Fact]
+		public async Task CreateTask_Return_Ok_When_Task_Created_With_Null_Description()
+		{
+			var mockTaskManager = new Mock<ITaskManager>();
+			mockTaskManager.Setup(m => m.CreateTaskAsync(It.IsAny<string>(), It.IsAny<string>()));
+			TaskController taskController = new(mockTaskManager.Object);
+
+			var result = await taskController.CreateTask("TestTask", null);
+
+			Assert.IsType<OkResult>(result);
+		}
+	}
+}
