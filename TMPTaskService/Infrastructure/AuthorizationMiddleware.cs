d1f23af0f9623a1b229379075d7c964c4be5ea0c
Nail Minnigalin <minnigalin.ni@finist-soft.ru>
2025-08-20 16:26:38 +0300

    fix integration tests

--- TMPTaskService/Infrastructure/AuthorizationMiddleware.cs
+++ TMPTaskService/Infrastructure/AuthorizationMiddleware.cs
@@ -2,17 +2,19 @@
 
 namespace TMPTaskService.Infrastructure
 {
-	public class AuthorizationMiddleware
+	public class AuthorizationMiddleware(RequestDelegate next, IHostEnvironment env)
 	{
-		private readonly RequestDelegate _nextDelegate;
-
-		public AuthorizationMiddleware(RequestDelegate next)
-		{
-			_nextDelegate = next;
-		}
+		private readonly RequestDelegate _nextDelegate = next;
+		private readonly IHostEnvironment _env = env;
 
 		public async Task InvokeAsync(HttpContext context, IAuthenticationManager authManager)
 		{
+			if (_env.IsEnvironment("IntegrationTest"))
+			{
+				await _nextDelegate(context);
+				return;
+			}
+
 			var endpoint = context.GetEndpoint();
 			if (endpoint == null)
 			{
